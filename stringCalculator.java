2c729b1c54cbfcfcf67a01142257ff2f8507db05
Parag Jadhav <parag@umstechlab.com>
2016-05-15 16:23:53 +0530

    committed java file

---
+++ stringCalculator.java
@@ -0,0 +1,39 @@
+package my.codekata;
+
+import java.util.Scanner;
+
+public class stringCalculator
+{
+	public static void main(String[] args)
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter Number:\n");
+
+		String input;
+		input = reader.nextLine();
+
+		int result = add(input);
+
+		System.out.println("Addition of the numbers is:\t"+result);
+		reader.close();
+	}//main
+
+	public static int add(String numbers)
+	{
+		int sum = 0;
+		if(numbers.equalsIgnoreCase(""))
+			sum = 0;
+		else if(numbers.length() == 1)
+		{		}//else-if
+		else if(numbers.contains(","))
+		{
+			String[] num = numbers.split(",");
+
+			for(String itr : num)
+			{
+				sum = sum+Integer.parseInt(itr);
+			}//for
+		}//else-if
+		return sum;
+	}//add
+}//stringCalculator
